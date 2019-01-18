4ca62c3828ae25b569f972c6e6d6686adb24556c
Torsten Regemortel <torsten.regemortel@evance.be>
2019-01-18 22:06:08 +0100

    updated packages and edited DataScroller.js so it uses value from props to render the items when using lazy loading

--- src/components/datascroller/DataScroller.js
+++ src/components/datascroller/DataScroller.js
@@ -4,220 +4,236 @@ import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 
 export class DataScroller extends Component {
-    
-    static defaultProps = {
-        id: null,
-        value: null,
-        rows: 0,
-        inline:	false,
-        scrollHeight: null,
-        loader:	null,
-        buffer: 0.9,
-        style: null,
-        className: null,
-        onLazyLoad: null,
-        itemTemplate: null,
-        header: null,
-        footer: null,
-        lazy: false
-    }
+  static defaultProps = {
+    id: null,
+    value: null,
+    rows: 0,
+    inline: false,
+    scrollHeight: null,
+    loader: null,
+    buffer: 0.9,
+    style: null,
+    className: null,
+    onLazyLoad: null,
+    itemTemplate: null,
+    header: null,
+    footer: null,
+    lazy: false
+  };
 
-    static propsTypes = {
-        id: PropTypes.string,
-        value: PropTypes.array,
-        rows: PropTypes.number,
-        inline: PropTypes.bool,
-        scrollHeight: PropTypes.any,
-        loader: PropTypes.any,
-        buffer: PropTypes.number,
-        style: PropTypes.object,
-        className: PropTypes.string,
-        onLazyLoad: PropTypes.func,
-        itemTemplate: PropTypes.func,
-        header: PropTypes.string,
-        footer: PropTypes.string,
-        lazy: PropTypes.bool
-    }
+  static propsTypes = {
+    id: PropTypes.string,
+    value: PropTypes.array,
+    rows: PropTypes.number,
+    inline: PropTypes.bool,
+    scrollHeight: PropTypes.any,
+    loader: PropTypes.any,
+    buffer: PropTypes.number,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    onLazyLoad: PropTypes.func,
+    itemTemplate: PropTypes.func,
+    header: PropTypes.string,
+    footer: PropTypes.string,
+    lazy: PropTypes.bool
+  };
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.dataToRender = [];
-        this.value = this.props.value;
-        this.first = 0;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.dataToRender = [];
+    this.value = this.props.value;
+    this.first = 0;
+  }
 
-    handleDataChange() {
-        if(this.props.lazy) {
-            this.dataToRender = this.value;
-            this.setState({ dataToRender: this.dataToRender });
-        }
-        else {
-            this.load();
-        }
+  handleDataChange() {
+    if (!this.props.lazy) {
+      this.load();
     }
-        
-    load() {
-        if(this.props.lazy) {
-            if(this.props.onLazyLoad) {
-                this.props.onLazyLoad(this.createLazyLoadMetadata());
-            }
-            
-            this.first = this.first + this.props.rows;
-        }
-        else {
-            if(this.value) {
-                for(var i = this.first; i < (this.first + this.props.rows); i++) {
-                    if(i >= this.value.length) {
-                        break;
-                    }
+  }
 
-                    this.dataToRender.push(this.value[i]);
-                }
-                
-                this.first = this.first + this.props.rows;
-                this.setState({ dataToRender: this.dataToRender });
-            }
+  load() {
+    if (this.props.lazy) {
+      if (this.props.onLazyLoad) {
+        this.props.onLazyLoad(this.createLazyLoadMetadata());
+      }
+    } else {
+      if (this.value) {
+        for (var i = this.first; i < this.first + this.props.rows; i++) {
+          if (i >= this.value.length) {
+            break;
+          }
+
+          this.dataToRender.push(this.value[i]);
         }
-    }
-     
-    reset() {
-        this.first = 0;
-        this.dataToRender = [];
+
+        this.first = this.first + this.props.rows;
         this.setState({ dataToRender: this.dataToRender });
+      }
+    }
+  }
+
+  reset() {
+    this.first = 0;
+    this.dataToRender = [];
+    this.setState({ dataToRender: this.dataToRender });
+    this.load();
+  }
+
+  isEmpty() {
+    return !this.dataToRender || this.dataToRender.length === 0;
+  }
+
+  createLazyLoadMetadata() {
+    const { value, rows } = this.props;
+    return {
+      first: value.length,
+      rows
+    };
+  }
+
+  bindScrollListener() {
+    if (this.props.inline) {
+      this.scrollFunction = () => {
+        var scrollTop = this.contentElement.scrollTop,
+          scrollHeight = this.contentElement.scrollHeight,
+          viewportHeight = this.contentElement.clientHeight;
+
+        if (scrollTop >= scrollHeight * this.props.buffer - viewportHeight) {
+          this.load();
+        }
+      };
+
+      this.contentElement.addEventListener('scroll', this.scrollFunction);
+    } else {
+      this.scrollFunction = () => {
+        var docBody = document.body,
+          docElement = document.documentElement,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+          winHeight = docElement.clientHeight,
+          docHeight = Math.max(
+            docBody.scrollHeight,
+            docBody.offsetHeight,
+            winHeight,
+            docElement.scrollHeight,
+            docElement.offsetHeight
+          );
+
+        if (scrollTop >= docHeight * this.props.buffer - winHeight) {
+          this.load();
+        }
+      };
+
+      window.addEventListener('scroll', this.scrollFunction);
+    }
+  }
+
+  unbindScrollListener() {
+    if (this.scrollFunction) {
+      if (this.props.inline) {
+        this.contentElement.removeEventListener('scroll', this.scrollFunction);
+        this.contentElement = null;
+      } else if (this.loader && this.isLoaded) {
+        this.loader.removeEventListener('click', this.scrollFunction);
+      } else {
+        window.removeEventListener('scroll', this.scrollFunction);
+      }
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.lazy) {
+      this.load();
+    }
+
+    if (this.props.loader) {
+      this.scrollFunction = () => {
         this.load();
+      };
+      this.loader = ReactDOM.findDOMNode(this.props.loader);
+      this.loader.addEventListener('click', this.scrollFunction);
+      this.isLoaded = true;
+    } else {
+      this.bindScrollListener();
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    var newValue = this.props.value;
+    if (newValue && this.value !== newValue) {
+      this.value = newValue;
+
+      this.handleDataChange();
     }
 
-    isEmpty() {
-        return !this.dataToRender||(this.dataToRender.length === 0);
+    if (this.props.loader && !this.isLoaded) {
+      this.unbindScrollListener();
+
+      this.scrollFunction = () => {
+        this.load();
+      };
+      this.loader = ReactDOM.findDOMNode(this.props.loader);
+      this.loader.addEventListener('click', this.scrollFunction);
+      this.isLoaded = true;
     }
-    
-    createLazyLoadMetadata() {
-        return {
-            first: this.first,
-            rows: this.props.rows
-        };
+  }
+
+  componentWillUnmount() {
+    if (this.scrollFunction) {
+      this.unbindScrollListener();
     }
-    
-    bindScrollListener() {
-        if(this.props.inline) {
-            this.scrollFunction = () => {
-                var scrollTop = this.contentElement.scrollTop,
-                    scrollHeight = this.contentElement.scrollHeight,
-                    viewportHeight = this.contentElement.clientHeight;
+  }
 
-                if((scrollTop >= ((scrollHeight * this.props.buffer) - (viewportHeight)))) {
-                    this.load();
-                }
-            }
-            
-            this.contentElement.addEventListener('scroll', this.scrollFunction);
-        }
-        else {
-            this.scrollFunction = () => {
-                var docBody = document.body,
-                    docElement = document.documentElement,
-                    scrollTop = (window.pageYOffset||document.documentElement.scrollTop),
-                    winHeight = docElement.clientHeight,
-                    docHeight = Math.max(docBody.scrollHeight, docBody.offsetHeight, winHeight, docElement.scrollHeight, docElement.offsetHeight);
-                            
-                if(scrollTop >= ((docHeight * this.props.buffer) - winHeight)) {
-                    this.load();
-                }
-            }
-
-            window.addEventListener('scroll', this.scrollFunction);
-        } 
+  renderElements = () => {
+    const { dataToRender } = this.state;
+    const { value, lazy, itemTemplate } = this.props;
+    if (lazy && value) {
+      return value.map((val, i) => (
+        <li key={i + '_datascrollitem'}>
+          {itemTemplate ? itemTemplate(val) : val}
+        </li>
+      ));
     }
+    return (
+      dataToRender &&
+      dataToRender.map((val, i) => {
+        var listItemContent = itemTemplate ? itemTemplate(val) : val;
+        return <li key={i + '_datascrollitem'}>{listItemContent}</li>;
+      })
+    );
+  };
 
-    unbindScrollListener() {
-        if (this.scrollFunction) {
-            if(this.props.inline) {
-                this.contentElement.removeEventListener('scroll', this.scrollFunction);
-                this.contentElement = null;
-            }
-            else if(this.loader && this.isLoaded) {
-                this.loader.removeEventListener('click', this.scrollFunction);
-            }
-            else {
-                window.removeEventListener('scroll', this.scrollFunction);
-            }   
-        }
-    }
+  render() {
+    var className = classNames(
+      'p-datascroller p-component',
+      this.props.className,
+      {
+        'p-datascroller-inline': this.props.inline
+      }
+    );
 
-    componentDidMount() {
-        if(this.props.lazy) {
-            this.load();
-        }
+    var header = this.props.header && (
+        <div className='p-datascroller-header'> {this.props.header}</div>
+      ),
+      footer = this.props.footer && (
+        <div className='p-datascroller-footer'> {this.props.footer} </div>
+      ),
+      content = (
+        <div
+          ref={el => (this.contentElement = ReactDOM.findDOMNode(el))}
+          className='p-datascroller-content'
+          style={{ maxHeight: this.props.scrollHeight }}
+        >
+          <ul className='p-datascroller-list'>{this.renderElements()}</ul>
+        </div>
+      );
 
-        if(this.props.loader) {
-            this.scrollFunction = () => {
-                this.load();
-            }
-            this.loader = ReactDOM.findDOMNode(this.props.loader);
-            this.loader.addEventListener('click', this.scrollFunction);
-            this.isLoaded = true;
-        }
-        else {
-            this.bindScrollListener();
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        var newValue = this.props.value;
-        if (newValue && this.value !== newValue) {
-            this.value = newValue;
-            
-            this.handleDataChange();
-        } 
-
-        if(this.props.loader && !this.isLoaded) {
-            this.unbindScrollListener();
-
-            this.scrollFunction = () => {
-                this.load();
-            }
-            this.loader = ReactDOM.findDOMNode(this.props.loader);
-            this.loader.addEventListener('click', this.scrollFunction);
-            this.isLoaded = true;
-        }
-    }
-
-    componentWillUnmount() {
-        if (this.scrollFunction) {
-            this.unbindScrollListener();
-        }
-    }
-
-    render() {
-        var className = classNames('p-datascroller p-component', this.props.className, {
-            'p-datascroller-inline': this.props.inline
-        });
-
-        var header =this.props.header && <div className="p-datascroller-header"> {this.props.header}</div>,
-            footer = this.props.footer && <div className="p-datascroller-footer"> {this.props.footer} </div>,
-            content = (
-                <div ref={(el) => this.contentElement = ReactDOM.findDOMNode(el)} className="p-datascroller-content" style={{'maxHeight': this.props.scrollHeight}}>
-                    <ul className="p-datascroller-list">
-                    {
-                        this.state.dataToRender && this.state.dataToRender.map((val, i) => {
-                            var listItemContent = this.props.itemTemplate ? this.props.itemTemplate(val) : val;
-                            return (<li key={i + '_datascrollitem'}>
-                                       {listItemContent}
-                                    </li>);
-                            })
-                    }
-                    </ul>
-                </div>
-            );
-
-        return (
-            <div id={this.props.id} className={className}>
-                {header}
-                {content}
-                {footer}
-            </div>
-        );
-    }
-
+    return (
+      <div id={this.props.id} className={className}>
+        {header}
+        {content}
+        {footer}
+      </div>
+    );
+  }
 }
